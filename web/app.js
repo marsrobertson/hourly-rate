fafc7c46edd16f3b314f1addd3559904d56a6eff
M <>
2020-01-31 17:52:01 +0000

    Reading from smart contract

--- web/app.js
+++ web/app.js
@@ -1,57 +1,58 @@
-var bncAssistConfig = {
-    dappId: "ef72f8d0-ca72-4571-98b1-b96e8c046c69",
-    networkId: 3, // Ropsten
-    web3: web3
-};
-var assistInstance = assist.init(bncAssistConfig);        
-assistInstance.onboard()
-    .then(async function(success) {
-        console.log("EVERYTHING OK, no action needed");
-        // User has been successfully onboarded and is ready to transact
-        // This means we can be sure of the follwing user properties:
-        //  - They are using a compatible browser
-        //  - They have a web3-enabled wallet installed
-        //  - The wallet is connected to the config-specified networkId
-        //  - The wallet is unlocked and contains at least `minimumBalance` in wei
-        //  - They have connected their wallet to the dapp, congruent with EIP1102
+async function init() {
 
-        ABI = [{"constant":true,"inputs":[],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"rate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_rate","type":"uint256"},{"name":"_validTo","type":"uint256"}],"name":"updateRate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isRateValid","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"validTo","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"kamikaze","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_ERC20TokenAddress","type":"address"},{"name":"_rate","type":"uint256"},{"name":"_validTo","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"ETH","type":"uint256"},{"indexed":false,"name":"tokens","type":"uint256"},{"indexed":false,"name":"rate","type":"uint256"}],"name":"Trade","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"rate","type":"uint256"}],"name":"ExchangeRateUpdated","type":"event"}]
-        address = "0x765eb7a0cb7ecb9cc555e8ad77d13396d1052c3c";
-        contractRaw = new web3.eth.Contract(ABI, address);
-        contract = assistInstance.Contract(contractRaw)
-        account = (await web3.eth.getAccounts())[0];
-    
-        validTo = await contract.methods.validTo().call(); 
-        $("#validTo").val(new Date(validTo * 1000).toUTCString());
+    // same ABI for both PNK and DAI (ERC20 interface, no need for all the "funky" methods)
+    ABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"amount","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"sender","type":"address"},{"name":"recipient","type":"address"},{"name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"},{"name":"amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"},{"name":"amount","type":"uint256"}],"name":"burnFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"addMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"renounceMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recipient","type":"address"},{"name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isMinter","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"name","type":"string"},{"name":"symbol","type":"string"},{"name":"decimals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterRemoved","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]
+    addressPNK = "0xec25392e13ea5a3a43bda3279d11ab533e7b12f0";
+    addressDAI = "0x311FABaE9000F246cf34Ac47FF3dE76ebAA3609A";
 
-        rate = await contract.methods.rate().call(); 
-        $("#rate").val(rate); 
-        $("#rateLabel").text(`Current rate 1 ETH = ${rate} ERC:`)
-        $("#eth").trigger("input");
-    })
-    .catch(function(error) {
-        // The user exited onboarding before completion
-        // Will let you know what stage of onboarding the user was up to when they exited
-        console.log(error.message);
-    })
+    PNK = new web3.eth.Contract(ABI, addressPNK);
+    DAI = new web3.eth.Contract(ABI, addressDAI);
 
-$("#trade").on("submit", async function(event) {
-    let wei = web3.utils.toWei( $("#eth").val() );
-    console.log("ETH: " +  $("#eth").val() + " " + wei);
-    contract.methods.kamikaze().send({value: wei, from: account})
-    event.preventDefault();
-    return false;
-})
+    account = (await web3.eth.getAccounts())[0];
+    console.log("Account from Metamask: " + account);
 
-$("#eth").on("input", function() {
-    let float = parseFloat($(this).val());
-    if (isNaN(float) || float < 0) {
-        float = 0; 
-        $(this).addClass("error"); 
-    } else { 
-        $(this).removeClass("error"); 
-    }
-    let pnk = rate * float;
-    console.log(`With ${float} you can get ${pnk.toFixed(2)} PNK`);
-    $("#pnk").val(pnk.toFixed(3));
-})
+    let balancePNKwei = await PNK.methods.balanceOf(account).call();
+    let balancePNK = parseFloat(web3.utils.fromWei(balancePNKwei)).toFixed(3);
+    $("#pnk").val(balancePNK);
+
+    let balanceDAIwei = await DAI.methods.balanceOf(account).call();
+    let balanceDAI = parseFloat(web3.utils.fromWei(balanceDAIwei)).toFixed(3);
+    $("#dai").val(balanceDAI);
+
+}
+
+
+
+
+
+
+
+// validTo = await contract.methods.validTo().call(); 
+// $("#validTo").val(new Date(validTo * 1000).toUTCString());
+
+// rate = await contract.methods.rate().call(); 
+// $("#rate").val(rate); 
+// $("#rateLabel").text(`Current rate 1 ETH = ${rate} ERC:`)
+// $("#eth").trigger("input");
+
+
+// $("#trade").on("submit", async function(event) {
+//     let wei = web3.utils.toWei( $("#eth").val() );
+//     console.log("ETH: " +  $("#eth").val() + " " + wei);
+//     contract.methods.kamikaze().send({value: wei, from: account})
+//     event.preventDefault();
+//     return false;
+// })
+
+// $("#eth").on("input", function() {
+//     let float = parseFloat($(this).val());
+//     if (isNaN(float) || float < 0) {
+//         float = 0; 
+//         $(this).addClass("error"); 
+//     } else { 
+//         $(this).removeClass("error"); 
+//     }
+//     let pnk = rate * float;
+//     console.log(`With ${float} you can get ${pnk.toFixed(2)} PNK`);
+//     $("#pnk").val(pnk.toFixed(3));
+// })
